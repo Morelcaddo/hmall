616a494916ac0879ec0b062f15e4ccaa7bbb1f1a
Apricity <SinpleTozzz@outlook.com>
2025-03-24 23:58:07 +0800

    2025-03-24 23:58

--- pay-service/src/main/java/com/hmall/pay/api/config/PayFeignConfig.java
+++ pay-service/src/main/java/com/hmall/pay/api/config/PayFeignConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Slf4j
-@Configuration
 public class PayFeignConfig {
     @Bean
     public PayClientFallback payClientFallback() {
--- user-service/src/main/java/com/hmall/user/api/config/UserFeignConfig.java
+++ user-service/src/main/java/com/hmall/user/api/config/UserFeignConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Slf4j
-@Configuration
 public class UserFeignConfig {
     @Bean
     public UserClientFallback userClientFallback() {
